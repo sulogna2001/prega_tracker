4ecac8b23fc64818ecb7e25d0815dc932343b08e
Sumeli Debdas <sumeli.debdas22@gmail.com>
2022-04-01 02:57:51 +0530

    doctor login and register

--- clientsite/src/App.js
+++ clientsite/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Register from './Components/Register/Register'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './Components/Login/Login';
+import ForgetPass from './Components/Forget Password/ForgetPass';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='forgetPassword' element={<ForgetPass />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ clientsite/src/Components/Register/Register.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import './Register.css'
+import { Link } from 'react-router-dom'
+import image1 from '../assets/Doctor_register.svg'
+import { Icon } from 'react-icons-kit'
+import { eye } from 'react-icons-kit/feather/eye'
+import { eyeOff } from 'react-icons-kit/feather/eyeOff'
+
+const Register = () => {
+  const [type, setType] = useState('password')
+  const [icon, setIcon] = useState(eyeOff)
+
+  const handleToggle = () => {
+    if (type === 'password') {
+      setIcon(eye)
+      setType('text')
+    }
+
+    else {
+      setIcon(eyeOff)
+      setType('password')
+    }
+  }
+
+  return (
+    <div className='register'>
+      <div className='register-box'>
+        <div className='register-row'>
+          <div className='register-col1'>
+            <img src={image1} className='register-img' />
+            <h1 className='doctor-text'>Welcome Doctors</h1>
+          </div>
+          <div className='register-col2'>
+            <div className='register-form-part'>
+              <h1>Let's Get Started</h1>
+              <form className='register-form'>
+                <input
+                  placeholder='Name'
+                  name='name'
+                  type='text'
+                />
+                <input
+                  placeholder='Email Address'
+                  name='email'
+                  type='email'
+                />
+                <div className='pass-section'>
+                  <input
+                    placeholder='Password'
+                    name='password'
+                    type={type}
+                  />
+                  <span onClick={handleToggle} className='pass-icon'><Icon icon={icon} size={20} /></span>
+                </div>
+                <div className='conpass-section'>
+                  <input
+                    placeholder='Confirm Password'
+                    name='confirm password'
+                    type={type}
+                  />
+                  <span onClick={handleToggle} className='conpass-icon'><Icon icon={icon} size={20} /></span>
+                </div>
+                <input
+                  placeholder='Phone Number'
+                  name='phone number'
+                  type='text'
+                />
+              </form>
+              <button className='register-button'>Register</button>
+              <p className='extra-text'>Alredy have an account?
+                <Link to="/login"> Log in</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
