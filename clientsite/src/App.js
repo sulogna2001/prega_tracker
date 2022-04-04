c202eccb5fc18714d17d81776832acfb792dd73d
sulognayehlosoft <sulogna.yehlosoft@gmail.com>
2022-04-04 14:55:44 +0530

    Resolved conflict app.js

--- clientsite/src/App.js
+++ clientsite/src/App.js
@@ -17,20 +17,7 @@ import PatientLogin from './Patients Components/Login/PatientLogin';
 import PatientForgetPass from './Patients Components/ForgetPassword/PatientForgetPass';
 import PatientResetPass from './Patients Components/ForgetPassword/PatientResetPass';
 import { DashboardPatient } from './Patients Components/PatientDashboard/DashboardPatient';
-import React from "react";
-import Register from "./Components/Register/Register";
-import { Routes, Route } from "react-router-dom";
-import Login from "./Components/Login/Login";
-import ForgetPass from "./Components/Forget Password/ForgetPass";
-import Dashboard from "./Components/Dashboard for Doctor/Dashboard";
-import ResetPass from "./Components/Forget Password/ResetPass";
-import Home from "./Components/Home/Home";
-import Overview from "./Components/OverviewDashboard/Overview/Overview";
-import LandingPage from "./Components/Landing Page/LandingPage";
-import PatientRegister from "./Patients Components/Register/PatientRegister";
-import PatientLogin from "./Patients Components/Login/PatientLogin";
-import PatientForgetPass from "./Patients Components/ForgetPassword/PatientForgetPass";
-import PatientResetPass from "./Patients Components/ForgetPassword/PatientResetPass";
+
 import AuthContextProvider from "./Context/AuthContext";
 import DoctorDetails from "./Components/Personal Doctor Detail/DoctorDetails";
 
@@ -47,6 +34,8 @@ const App = () => {
           <Route path='/reset/:verificationtoken/:id' element={<ResetPass />}/>
           <Route path='/doctorDetails' element={<Dashboard/>} />
           <Route path='/doctorDashboard' element={<Overview />} />
+          <Route path="/doctor/:id" element={<DoctorDetails />} />
+
 
           //For Patients
           <Route path='/patientRegister' element={<PatientRegister />} />
@@ -54,21 +43,9 @@ const App = () => {
           <Route path='/patientForgetPass' element={<PatientForgetPass />} />
           <Route path='/patientResetPass' element={<PatientResetPass />} />
           <Route path ='/patientdashboard' element={<DashboardPatient/>}/>
-        {/* //For Doctors */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/doctorRegister" element={<Register />} />
-        <Route path="/doctorlogin" element={<Login />} />
-        <Route path="/forgetPassword" element={<ForgetPass />} />
-        <Route path="/reset/:verificationtoken/:id" element={<ResetPass />} />
-        <Route path="/doctorDetails" element={<Dashboard />} />
-        <Route path="/doctorDashboard" element={<Overview />} />
-        <Route path="/doctor/:id" element={<DoctorDetails />} />
 
-        {/* //For Patients */}
-        <Route path="/patientRegister" element={<PatientRegister />} />
-        <Route path="/patientLogin" element={<PatientLogin />} />
-        <Route path="/patientForgetPass" element={<PatientForgetPass />} />
+
+       
         <Route
           path="/patientreset/:verificationtoken/:id"
           element={<PatientResetPass />}
