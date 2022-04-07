5b1bed0d5d338c4186e4b8e6e7b8ac5490dee0a4
sulogna2001 <roysulogna2001@gmail.com>
2022-04-07 17:01:08 +0530

    added

--- clientsite/src/Components/Chat/Chat.js
+++ clientsite/src/Components/Chat/Chat.js
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import NavbarAll from "../Navbar/Navbar";
+import { CssBaseline } from "@mui/material";
+import ChatMain from "./ChatMain";
 export const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div>
+      <>
+        <CssBaseline />
+          <NavbarAll />
+        <div
+          className="container-fluid computer"
+          style={{ overflow: "hidden" }}
+        >
+          <div className="row">
+            <div className="col-1  px-1 " id="main"></div>
+            <div className="col-10  px-3 " id="main">
+              {/* <ChatMain/> */}
+            </div>
+            <div className="col-1 offset-3 " id="main"></div>
+          </div>
+        </div>
+        <div style={{ display: "none" }} className="mediadev">
+          {/* Mobile View */}
+          
+        </div>
+      </>
+    </div>
+  );
+};
