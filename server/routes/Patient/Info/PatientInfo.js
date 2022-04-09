6900a19c201b74b357baba93509b318115da4560
sulogna2001 <roysulogna2001@gmail.com>
2022-04-09 19:09:16 +0530

    Added

--- server/routes/Patient/Info/PatientInfo.js
+++ server/routes/Patient/Info/PatientInfo.js
@@ -1,12 +1,20 @@
-const {getPatientInfo ,getPatientById} = require('../../../controllers/Patient/Info/GetPatient');
-const {updatePatientInfo}= require('../../../controllers/Patient/Info/UpdatePatient');
+const {
+  getPatientInfo,
+  getPatientById,
+  getAllDocOfAPatient,
+} = require("../../../controllers/Patient/Info/GetPatient");
+const {
+  updatePatientInfo,
+} = require("../../../controllers/Patient/Info/UpdatePatient");
 
 const router = require("express").Router();
 
-const {verifyPatientJWT}=require("../../../middlewares/Patient/VerifyJwt");
+const { verifyPatientJWT } = require("../../../middlewares/Patient/VerifyJwt");
 
-router.get("/patientinfo",verifyPatientJWT,getPatientInfo);
-router.put("/updatepatient", verifyPatientJWT , updatePatientInfo);
-router.get("/:id" ,getPatientById);
+router.get("/patientinfo", verifyPatientJWT, getPatientInfo);
+router.put("/updatepatient", verifyPatientJWT, updatePatientInfo);
+router.get("/getdo/",verifyPatientJWT, getAllDocOfAPatient);
 
-module.exports = router
+router.get("/:id", getPatientById);
+
+module.exports = router;
